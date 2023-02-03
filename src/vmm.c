2e730eacc3903f72a7fa7d22dc9f23efffef5147
Matt <933056+iblazys@users.noreply.github.com>
2023-02-02 23:01:31 -0500

    allocate guest state and get processor count

--- src/vmm.c
+++ src/vmm.c
@@ -1,21 +1,61 @@
-#include <linux/kernel.h> /* Needed for pr_info() */ 
+/* 
+ * vmm.c - our virtual machine manager
+ */ 
+
+#include <linux/kernel.h>
+#include <linux/slab.h> // memory alloc
+#include <linux/gfp.h> // memory alloc flags
+
+#include <cpuid.h> // cpuid intrinsic
+#include <asm/msr.h> // msr instrinsic
+
+#include "../ia32-doc/out/ia32.h"
+//#include "asm_operations.h"
 #include "vmm.h"
 
-#include "../include/ia32-doc/out/ia32.h" // will need this in almost every file
-
 bool InitVMM(void)
-{
-    if(!IsVMXSupported()) 
+{        
+    if(!CheckCPUFeatures()) 
     {
-        pr_info("Your processor does not support vmx");
+        // Handled by above function. 
+        return false;
+    }
+
+    pr_info("VMX support detected");
+
+    int32_t processorCount = num_online_cpus();
+
+    // Allocate guest state (virtual cpu state) array
+    // TODO: Create guest state struct
+    VIRTUAL_CPU_STATE *guestState = kzalloc(sizeof(VIRTUAL_CPU_STATE) * processorCount, GFP_NOWAIT);
+
+    pr_info("Allocated guest state for %d processors", processorCount);
+
+    kfree(guestState);
+
+    pr_info("Freed guest state");
+
+    return true;
+}
+
+/// @brief Check if the cpu supports the features required by this hypervisor.
+bool CheckCPUFeatures(void)
+{
+    CPUID_EAX_01 cpu = { 0 };
+
+    // Run the cpuid instruction
+    __get_cpuid(1, &cpu.CpuidVersionInformation.AsUInt,
+        &cpu.CpuidAdditionalInformation.AsUInt,
+        &cpu.CpuidFeatureInformationEcx.AsUInt,
+        &cpu.CpuidFeatureInformationEdx.AsUInt);
+
+    // bit 31 of ecx = hypervisor present bit
+    
+    if (cpu.CpuidFeatureInformationEcx.VirtualMachineExtensions == 0) 
+    {
+        pr_info("VMX is not supported on your processor.");
         return false;
     }
     
     return true;
-}
-
-bool IsVMXSupported(void)
-{
-
-    return false;
 }
--- src/vmm.h
+++ src/vmm.h
@@ -1,3 +1,24 @@
+#ifndef LINUXHV_VMM_H
+#define LINUXHV_VMM_H
+
+#include <linux/kernel.h>
+
+typedef struct _VIRTUAL_CPU_STATE
+{
+    bool IsOnVmxRootMode;
+    bool IncrementRip;
+
+    uint64_t VmxonRegionPhysicalAddress;
+    uint64_t VmxonRegionVirtualAddress;
+    uint64_t VmcsRegionPhysicalAddress;	
+    uint64_t VmcsRegionVirtualAddress;
+    uint64_t MsrBitmapVirtualAddress; 
+    uint64_t MsrBitmapPhysicalAddress;
+                                                    
+    uint64_t VmmStack;                                    
+} VIRTUAL_CPU_STATE, * PVIRTUAL_CPU_STATE;
 
 bool InitVMM(void);
-bool IsVMXSupported(void);
+bool CheckCPUFeatures(void);
+
+#endif
