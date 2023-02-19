94e3f314bca1209f5f667f13fd7e40e3b7b41976
Matt <933056+iblazys@users.noreply.github.com>
2023-02-19 12:56:25 -0500

    handle invalid guest state entry

--- include/exit.h
+++ include/exit.h
@@ -2,8 +2,63 @@
 #define __LINUXHV_EXIT_H
 
 #include "x86.h"
+#include "vmm.h"
+
+#define STACK_REG_AX			0
+#define STACK_REG_CX			1
+#define STACK_REG_DX			2
+#define STACK_REG_BX			3
+#define STACK_REG_SP			4
+#define STACK_REG_BP			5
+#define STACK_REG_SI			6
+#define STACK_REG_DI			7
+#define STACK_REG_R8			8
+#define STACK_REG_R9			9
+#define STACK_REG_R10			10
+#define STACK_REG_R11			11
+#define STACK_REG_R12			12
+#define STACK_REG_R13			13
+#define STACK_REG_R14			14
+#define STACK_REG_R15			15
+#define STACK_EFL_VCPU			16
+#define STACK_VCPU              17
 
 bool exit_handle_vmexit(uintptr_t *stack);
 void exit_handle_fail(uintptr_t *stack);
+
+/* should be called by a vmcall */
+static inline void exit_handle_vcpu_exit(struct virtual_cpu* vcpu)
+{
+    /* Fix GDT  
+	struct gdtr gdt = {
+		.limit = (u16)vmcs_read32(GUEST_GDTR_LIMIT),
+		.base = vmcs_read(GUEST_GDTR_BASE),
+	};
+	__lgdt(&gdt);
+    */
+
+	/* Fix IDT (restore whatever guest last loaded...) 
+	__lidt(&vcpu->g_idt);
+    */
+
+	uintptr_t ret = vcpu->rip + vmread(VMCS_VMEXIT_INSTRUCTION_LENGTH);
+
+    // set rflags
+	//vcpu_vm_succeed(vcpu);
+
+	uintptr_t cr3 = vmread(VMCS_GUEST_CR3);
+	write_cr3(cr3); // todo: wrapper __writec
+
+	/* See __vmx_entrypoint in assembly on how this is used.  */
+    *(uintptr_t *)&vcpu->host_sp[STACK_REG_CX] = ret;
+    *(uintptr_t *)&vcpu->host_sp[STACK_REG_DX] = vcpu->host_sp[STACK_REG_SP];
+    *(uintptr_t *)&vcpu->host_sp[STACK_REG_AX] = vcpu->rflags;
+
+    /*
+	ksm_write_reg(vcpu, STACK_REG_CX, ret);
+	ksm_write_reg(vcpu, STACK_REG_DX, ksm_read_reg(vcpu, STACK_REG_SP));
+	ksm_write_reg(vcpu, STACK_REG_AX, vcpu->eflags);
+    */
+}
 
 #endif
--- src/exit.c
+++ src/exit.c
@@ -1,10 +1,61 @@
 #include "exit.h"
+#include "validate.h"
 
 bool exit_handle_vmexit(uintptr_t *stack)
 {
-     pr_info("VMEXIT HANDLER");
+    size_t exit_reason;
+    exit_reason = 0;
 
-     return true;
+    struct virtual_cpu* vcpu = (struct virtual_cpu *)stack[STACK_EFL_VCPU];
+
+    vcpu->host_sp = stack;
+	vcpu->host_sp[STACK_REG_SP] = vmread(VMCS_GUEST_RSP);
+	vcpu->rflags = vmread(VMCS_GUEST_RFLAGS);
+	vcpu->rip = vmread(VMCS_GUEST_RIP);
+
+    __vmx_vmread(VMCS_EXIT_REASON, &exit_reason);
+    
+    exit_reason &= 0xFFFF;
+
+    pr_info("linux-hv: vmexit on cpu %d with exit reason 0x%zx", vcpu->processor_id, exit_reason);
+
+    switch (exit_reason)
+    {
+        case VMX_EXIT_REASON_EXECUTE_VMCLEAR:
+        case VMX_EXIT_REASON_EXECUTE_VMPTRLD:
+        case VMX_EXIT_REASON_EXECUTE_VMPTRST:
+        case VMX_EXIT_REASON_EXECUTE_VMREAD:
+        case VMX_EXIT_REASON_EXECUTE_VMRESUME:
+        case VMX_EXIT_REASON_EXECUTE_VMWRITE:
+        case VMX_EXIT_REASON_EXECUTE_VMXOFF:
+        case VMX_EXIT_REASON_EXECUTE_VMXON:
+        case VMX_EXIT_REASON_EXECUTE_VMLAUNCH:
+        {
+            pr_info("VMEXIT: VMXINSTRUCTION");
+            break;
+        }
+
+        case VMX_EXIT_REASON_EXECUTE_CPUID:
+        {
+            pr_info("VMEXIT: CPUID");
+            //exit_handle_cpuid();
+            break;
+        }
+
+        case VMX_EXIT_REASON_ERROR_INVALID_GUEST_STATE:
+        {
+            pr_info("entry with invalid guest state!");
+            validate_guest_entry_state();
+            exit_handle_vcpu_exit(vcpu);
+            return false;
+
+            break;
+        }
+    }
+
+    // returning false = vmxoff
+    // use a VMCALL to turn it off
+    return true;
 }
 
 void exit_handle_fail(uintptr_t *stack)
